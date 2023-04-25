ceb5e4f57fde58b951df40d65e03a593bd066163
Abdur <rahmanabdurr65@gmail.com>
2023-04-24 22:38:59 -1100

    initial commit

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { Container,Col,Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Pages/Footer/Footer';
+
 import LeftSideNav from '../Pages/LeftSideNav/LeftSideNav';
 import Header from '../Pages/Shared/Header/Header';
 import RightSide from '../Pages/RightSideNav/RightSideNav'
-import { useState } from 'react';
+
 import { DarkModeContext } from '../Pages/theme/theme';
 
 const Main = () => {
